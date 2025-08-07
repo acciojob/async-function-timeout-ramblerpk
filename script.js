0cbc5bce237f46e2e6116a9bb1a2f05012bbdb18
Piyush Kumar <109018436+ramblerpk@users.noreply.github.com>
2025-08-07 14:31:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+document.getElementById('btn').addEventListener('click', async function(){
+	const message = document.getElementById('text').value;
+	const delay = parseInt(document.getElementById('delay').value);
+	await display(message,delay);
+});
+
+function display(message,delay) {
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+			document.getElementById('output').innerHTML = message;
+		},delay);
+	});
+}
